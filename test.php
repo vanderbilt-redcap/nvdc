894fe11c0f16f6119539ae6c659b8eabd4ac44dc
Carl Reed <carl.w.reed@vumc.org>
2019-02-25 14:34:49 -0600

    changed test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 // exit($module->printRandomMB());
 // $module->removeAttachedFiles();
-$module->attachFakeFiles();
+// $module->attachFakeFiles();
 
 // example file names:
 // Alarm history Export 7 days Infinity Acute Care System Workstation Critical Care Build 7016 ASEB-0069 01-Feb-2019 16_06_02.txt
