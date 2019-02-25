f3d3ef89740fc0a810c92da879aea3602b877f49
Carl Reed <carl.w.reed@vumc.org>
2019-02-25 14:51:57 -0600

    changes

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-// exit($module->printRandomMB());
 // $module->removeAttachedFiles();
 // $module->attachFakeFiles();
 
@@ -8,8 +7,7 @@
 // Logbook Export 7 days Infinity Acute Care System Workstation Critical Care Build 7016 ASCN-0001 01-Feb-2019 16_19_34.txt
 // Trends Export 7 days Infinity Acute Care System Workstation Critical Care Build 7016 ASCN-0001 01-Feb-2019 16_19_41.csv	
 
-// or:
-
 // Alarm ASCN-0001 25-Feb-2019 16_06_02.txt
 // Logbook ASCN-0001 25-Feb-2019 16_06_02.txt
 // Trends ASCN-0001 25-Feb-2019 16_06_02.txt
+
