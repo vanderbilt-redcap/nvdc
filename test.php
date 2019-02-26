01dee4e70597fd676e8515c34fc3881e074fa07a
Carl Reed <carl.w.reed@vumc.org>
2019-02-26 13:25:34 -0600

    update

--- test.php
+++ test.php
@@ -1,40 +1,68 @@
 <?
-$zipfilename = "zip_file_name.zip";
-
-if( isset( $files ) ) unset( $files );
-
-// $target = "/some/directory/of/files/you/want/to/zip";
-$target = $module->getModulePath();
-
-$d = dir( $target );
-
-while( false !== ( $entry = $d->read() ) )
-{
-	if( substr( $entry, 0, 1 ) != "." && !is_dir( $entry ) ) 
-	{
-		$files[] = $entry;
-	}
+$files = array($module->getModulePath() . "test.php");
+//Getting the total size of the files array
+$totalSize = 0;
+foreach ($files as $file) {
+	$totalSize += filesize($file);
+}
+$totalSize += 300; //I don't understand why, but the totalSize is always 300kb short of the correct size
+header('Pragma: no-cache'); 
+header('Content-Description: File Download'); 
+header('Content-disposition: attachment; filename="myZip.zip"');
+header('Content-Type: application/octet-stream');
+header('Content-Length: ' . $totalSize);
+header('Content-Transfer-Encoding: binary'); 
+//Opening a zip stream
+$files = implode(" ", $files);
+if ($files){
+	$fp = popen('zip -r -0 - ' . $files, 'r');
 }
 
-header( "Content-Type: application/x-zip" );
-header( "Content-Disposition: attachment; filename=\"$zipfilename\"" );
-
-$filespec = "";
-
-foreach( $files as $entry )
-{
-	$filespec .= "\"$entry\" ";
+flush(); //Flushing the butter, pre streaming
+while(!feof($fp)) {
+	echo fread($fp, 8192);
+}
+//Closing the stream
+if ($files){ 
+	pclose($fp);
 }
 
-chdir( $target );
+// $zipfilename = "zip_file_name.zip";
 
-$stream = popen( "/usr/bin/zip -q - $filespec", "r" );
+// if( isset( $files ) ) unset( $files );
 
-if( $stream )
-{
-	fpassthru( $stream );
-	fclose( $stream );
-}
+// // $target = "/some/directory/of/files/you/want/to/zip";
+// $target = $module->getModulePath();
+
+// $d = dir( $target );
+
+// while( false !== ( $entry = $d->read() ) )
+// {
+	// if( substr( $entry, 0, 1 ) != "." && !is_dir( $entry ) ) 
+	// {
+		// $files[] = $entry;
+	// }
+// }
+
+// header( "Content-Type: application/x-zip" );
+// header( "Content-Disposition: attachment; filename=\"$zipfilename\"" );
+
+// $filespec = "";
+
+// foreach( $files as $entry )
+// {
+	// $filespec .= "\"$entry\" ";
+// }
+
+// chdir( $target );
+
+// $stream = popen( "zip -q - $filespec", "r" );
+
+// if( $stream )
+// {
+	// fpassthru( $stream );
+	// fclose( $stream );
+// }
 
 // $module->removeAttachedFiles();
 // $module->attachFakeFiles();
