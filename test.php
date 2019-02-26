8e44d79cd7a2889ed03823e805704fd3ae01ddf1
Carl Reed <carl.w.reed@vumc.org>
2019-02-26 12:13:55 -0600

    update

--- test.php
+++ test.php
@@ -11,21 +11,51 @@
 // Logbook ASCN-0001 25-Feb-2019 16_06_02.txt
 // Trends ASCN-0001 25-Feb-2019 16_06_02.txt
 
-// $descriptorspec = array(
-    // 0 => array("pipe", "r"),
-    // 1 => array("pipe", "w"),
-    // 2 => array("file", "err.txt", "a")
-// );
-// // Create child and start process
-// $child = array("process" => null, "pipes" => null);
-// $child["process"] = proc_open("zip test.zip test.php", $descriptorspec, $child["pipes"]);
-// echo(stream_get_contents($child['pipes'][0]) . "<br />");
-// echo(stream_get_contents($child['pipes'][1]) . "<br />");
-// echo(file_get_contents("err.txt") . "<br />");
-// echo(gettype($child['process']) . "<br />");
-// echo(gettype($child['pipes']) . "<br />");
-// exit;
+// error_reporting(E_ALL);
+// $windows = strpos(php_uname(), "Windows") !== false;
+// $path1 = "\"C:\\\Program Files\\7-Zip\\7z.exe\"";
+// $path2 = "C:\\xampp\\htdocs\\redcap\\modules\\nvdc_v1.0\\";
+// $path3 = "~/www/redcap/modules/nvdc_v1.0/test.zip";
+// $path4 = "~/www/redcap/modules/nvdc_v1.0/test.php";
+// $cmd = $windows ? "$path1 a -tzip $path2" . "test.zip $path2" . "test.php" : "zip $path3 $path4";
+// $pathErr = $windows ? "C:\\xampp\\htdocs\\redcap\\modules\\nvdc_v1.0\\err.txt" : "~/www/redcap/modules/nvdc_v1.0/err.txt";
+// $spec = [
+	// 0 => ["pipe", 'r'],
+	// 1 => ["pipe", 'w'],
+	// 2 => ["file", $pathErr, 'a']
+// ];
+// $process = proc_open($cmd, $spec, $pipes);
+// echo "\$process: ";
+// var_dump($process);
+// echo "<br />";
+// echo "<br />";
+// echo "\$pipes[1]: ";
+// echo stream_get_contents($pipes[1]);
+// fclose($pipes[1]);
+// echo "<br />";
+// echo "<br />";
+// echo("err.txt: " . file_get_contents("err.txt"));
+// echo "<br />";
+// echo "<br />";
+// echo("stderr: " . file_get_contents("php://stderr"));
+// $returnVal = proc_close($process);
+// echo "<br />";
+// echo "<br />";
+// echo("\$returnVal: " . print_r($returnVal, true));
 
 error_reporting(E_ALL);
-exec('zip test.zip test.php', $out, $ret);
-exit($out . "<br />" . $ret . "<br />" . file_get_contents("php://stderr"));
+$modPath = "~/www/redcap/modules/nvdc_v1.0/";
+$command = "zip $modPath" . "NICU_Ventilator_Data_Files.zip $modPath" . "test.php";
+$spec = [
+	0 => ["pipe", 'r'],
+	1 => ["pipe", 'w'],
+	2 => ["file", $modPath . "err.txt", 'a']
+];
+$process = proc_open($command, $spec, $pipes);
+header('Content-Type: application/octet-stream');
+// header('Content-Description: File Transfer');
+header('Content-Disposition: attachment; filename=NICU_Ventilator_Data_Files.zip');
+while (false !== ($buffer = stream_get_contents($pipes[1]))) {
+	echo $buffer;
+}
+proc_close($process);
