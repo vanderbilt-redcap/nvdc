36ba7b9657b15127a8a6366bdd5a36565312a65b
Carl Reed <carl.w.reed@vumc.org>
2019-02-26 10:35:56 -0600

    update

--- test.php
+++ test.php
@@ -11,17 +11,20 @@
 // Logbook ASCN-0001 25-Feb-2019 16_06_02.txt
 // Trends ASCN-0001 25-Feb-2019 16_06_02.txt
 
-$descriptorspec = array(
-    0 => array("pipe", "r"),
-    1 => array("pipe", "w"),
-    2 => array("file", "err.txt", "a")
-);
-// Create child and start process
-$child = array("process" => null, "pipes" => null);
-$child["process"] = proc_open("zip test.zip test.php", $descriptorspec, $child["pipes"]);
-echo(stream_get_contents($child['pipes'][0]) . "<br />");
-echo(stream_get_contents($child['pipes'][1]) . "<br />");
-echo(file_get_contents("err.txt") . "<br />");
-echo(gettype($child['process']) . "<br />");
-echo(gettype($child['pipes']) . "<br />");
-exit;
+// $descriptorspec = array(
+    // 0 => array("pipe", "r"),
+    // 1 => array("pipe", "w"),
+    // 2 => array("file", "err.txt", "a")
+// );
+// // Create child and start process
+// $child = array("process" => null, "pipes" => null);
+// $child["process"] = proc_open("zip test.zip test.php", $descriptorspec, $child["pipes"]);
+// echo(stream_get_contents($child['pipes'][0]) . "<br />");
+// echo(stream_get_contents($child['pipes'][1]) . "<br />");
+// echo(file_get_contents("err.txt") . "<br />");
+// echo(gettype($child['process']) . "<br />");
+// echo(gettype($child['pipes']) . "<br />");
+// exit;
+
+exec('zip test.zip test.php');
+exit();
