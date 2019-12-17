8e3bec09137ca29887ae1254dfa56bf41c124a07
moorejr5 <james.r.moore@vanderbilt.edu>
2019-12-17 11:45:51 -0600

    Adding record ID to file names. ALlowing for record ID filtering.

--- NVDC.php
+++ NVDC.php
@@ -145,11 +145,9 @@ class NVDC extends \ExternalModules\AbstractExternalModule {
 		if (file_exists($zipFilePath)) unlink($zipFilePath);
 		$zip = new \ZipArchive();
 		$zip->open($zipFilePath, \ZipArchive::CREATE);
-		echo "<pre>";
-		print_r($edocs);
-		echo "</pre>";
+
 		foreach ($edocs as $edoc) {
-			$zip->addFile($edoc['filepath'], $edoc['mrn'] . ' ' . $edocs['record'] . ' ' . $edoc['filename']);
+			$zip->addFile($edoc['filepath'], $edoc['mrn'] . ' ' . $edoc['record'] . ' ' . $edoc['filename']);
 		}
 		$zip->close();
 	}
