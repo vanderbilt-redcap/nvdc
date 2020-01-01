c3da544ef36497c7c72cae65722e7a4ac4f9052d
moorejr5 <james.r.moore@vanderbilt.edu>
2020-01-01 14:06:47 -0600

    Testing cron with folder not already existing.

--- cron.php
+++ cron.php
@@ -17,6 +17,9 @@ print_r($edocs);
 echo "</pre>";
     $zipPath = $module::ORI_PATH;
 echo "Zip Path: $zipPath<br/>";
+if (!is_dir($zipPath)) {
+    mkdir($zipPath,'0755',true);
+}
     $module->createZipFile($zipPath . "NVDC_All_Files_" . $project_id.".zip", $edocs);
 }
 unset($_GET['pid']);
