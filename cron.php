ad1773436051a5b7c2e19acd3057472d0118348a
moorejr5 <james.r.moore@vanderbilt.edu>
2020-01-01 14:37:41 -0600

    Cleaning out folder of files.

--- cron.php
+++ cron.php
@@ -1,6 +1,7 @@
 <?php
 
 $projectList = $module->framework->getProjectsWithModuleEnabled();
+$zipPath = $module::ORI_PATH;
 
 foreach ($projectList as $project_id) {
     $_GET['pid'] = $project_id;
@@ -9,8 +10,6 @@ foreach ($projectList as $project_id) {
 
     $edocs = $mrnList["edocs"];
 
-    $zipPath = $module::ORI_PATH;
-
 /*if (!is_dir($zipPath)) {
     mkdir($zipPath,0755,true);
 }
@@ -18,3 +17,22 @@ chmod($zipPath,0755);*/
     $module->createZipFile($zipPath . "NVDC_All_Files_" . $project_id.".zip", $edocs);
 }
 unset($_GET['pid']);
+
+delete_files($zipPath);
+
+/*
+ * php delete function that deals with directories recursively
+ */
+function delete_files($target) {
+    if(is_dir($target)){
+        $files = glob( $target . '*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
+
+        foreach( $files as $file ){
+            delete_files( $file );
+        }
+
+        rmdir( $target );
+    } elseif(is_file($target)) {
+        unlink( $target );
+    }
+}
