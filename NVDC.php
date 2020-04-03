d9de8c700fde79d114200b764b8601a9328592f5
Carl Reed <carlreedw@gmail.com>
2020-04-03 12:12:29 -0500

    more error reporting

--- NVDC.php
+++ NVDC.php
@@ -177,9 +177,26 @@ class NVDC extends \ExternalModules\AbstractExternalModule {
         if (file_exists($zipFilePath)) unlink($zipFilePath);
         $zip = new \ZipArchive();
         
-		$res = $zip->open($zipFilePath, \ZipArchive::CREATE); 
-		if ($res !== true) {
-			return $res;
+		$res = $zip->open($zipFilePath, \ZipArchive::CREATE);
+		switch ($res) {
+			case \ZipArchive::ER_EXISTS:
+				return "File already exists.";
+			case \ZipArchive::ER_INCONS:
+				return "Zip archive inconsistent.";
+			case \ZipArchive::ER_INVAL:
+				return "Invalid argument.";
+			case \ZipArchive::ER_MEMORY:
+				return "Malloc failure.";
+			case \ZipArchive::ER_NOENT:
+				return "No such file.";
+			case \ZipArchive::ER_NOZIP:
+				return "Not a zip archive.";
+			case \ZipArchive::ER_OPEN:
+				return "Can't open file.";
+			case \ZipArchive::ER_READ:
+				return "Read error.";
+			case \ZipArchive::ER_SEEK:
+				return "Seek error.";
 		}
 		
         foreach ($edocs as $edoc) {
